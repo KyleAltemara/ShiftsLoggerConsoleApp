5bf9857f3e81ea9ff0a8036acd878226f0d11953
Kyle <Kyle.Altemara@gmail.com>
2024-05-30 11:06:40 -0700

    Created solution

---
+++ ShiftsLoggerConsoleApp/Models/ShiftLog.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiftsLoggerConsoleApp.Models;
+
+public class ShiftLog
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime ShiftStartTime { get; set; }
+    public DateTime ShiftEndTime { get; set; }
+}
